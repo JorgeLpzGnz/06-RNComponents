42872ca3e0324ce3243a55a63315292943f9521a
JorgeLpzGnz <geolg763@gmail.com>
2023-04-21 15:29:52 -0500

    191. FlatList P2 ( HeaderComponent, Separator )

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
+import { styles } from '../theme/appTheme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 interface MenuItem {
     name: string
@@ -13,28 +15,57 @@ const menuItems = [
         name: 'Animation 101',
         icon: 'cube-outline',
         components: 'Animation101Screen'
+    },
+    {
+        name: 'Animation 102',
+        icon: 'albums-outline',
+        components: 'Animation102Screen'
     }
 ]
 
 export const HomeScreen = () => {
 
-    const renderMenuItem = ( menuItem: MenuItem) => {
-        return (
-            <View>
-                <Text>{ menuItem.name } - { menuItem.icon }</Text>
-            </View>
-        )
-    }
+    const { top } = useSafeAreaInsets()
+
+    const renderMenuItem = (menuItem: MenuItem) => (
+        <View>
+            <Text>{menuItem.name} - {menuItem.icon}</Text>
+        </View>
+    )
+    
+
+    const renderListHeader = () => (
+            
+        <View style={{ marginTop: top + 20, marginBottom: 20, }}>
+            <Text style={ styles.title }>Opciones de menu</Text>
+        </View>
+
+    )
+
+    const itemSeparator = () => (
+        <View 
+            style={{
+                borderBottomWidth: 1,
+                opacity: 0.4,
+                marginVertical: 8,
+            }}
+        />
+    )
+
+    
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, ...styles.globalMargin }}>
+
             <FlatList
                 // arreglo con los elementos a mostrar
                 data={menuItems}
                 // funcion a la que tocar devolverle un elemento JSX
                 // Este elemto en el que va a renderizar la info de la data
-                renderItem={ ({item}) => renderMenuItem( item ) }
+                renderItem={({ item }) => renderMenuItem(item)}
                 // llave unica para cada elemnto del flatList ( tipo String )
-                keyExtractor={ (item) => item.name }
+                keyExtractor={(item) => item.name}
+                ListHeaderComponent={ renderListHeader }
+                ItemSeparatorComponent={ itemSeparator }
             />
         </View>
     )
