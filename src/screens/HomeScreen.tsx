324f5434b92ff26702c9e0ea84638b17320e120b
JorgeLpzGnz <geolg763@gmail.com>
2023-04-21 15:12:31 -0500

    190. FlatList P1

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
+import { FlatList } from 'react-native-gesture-handler'
+
+interface MenuItem {
+    name: string
+    icon: string
+    components: string
+}
+
+const menuItems = [
+    {
+        name: 'Animation 101',
+        icon: 'cube-outline',
+        components: 'Animation101Screen'
+    }
+]
 
 export const HomeScreen = () => {
+
+    const renderMenuItem = ( menuItem: MenuItem) => {
+        return (
+            <View>
+                <Text>{ menuItem.name } - { menuItem.icon }</Text>
+            </View>
+        )
+    }
     return (
-      <View>
-        <Text>HomeScreen</Text>
-        <Icon name='star-outline' size={20}/>
-      </View>
+        <View style={{ flex: 1 }}>
+            <FlatList
+                // arreglo con los elementos a mostrar
+                data={menuItems}
+                // funcion a la que tocar devolverle un elemento JSX
+                // Este elemto en el que va a renderizar la info de la data
+                renderItem={ ({item}) => renderMenuItem( item ) }
+                // llave unica para cada elemnto del flatList ( tipo String )
+                keyExtractor={ (item) => item.name }
+            />
+        </View>
     )
 }
