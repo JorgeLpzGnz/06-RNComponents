cc9065a2b01a7946738f7105333667066f200b1e
JorgeLpzGnz <geolg763@gmail.com>
2023-04-21 16:31:36 -0500

    197. useAnimation ( custom Hook )

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,42 +1,10 @@
 import React, { useRef } from 'react'
 import { Text, StyleSheet, View, Animated, Button, Easing } from 'react-native'
+import { useAnimation } from '../Hooks/useAnimation'
 
 export const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0)).current
-    const top = useRef(new Animated.Value(-100)).current
-
-    const fadeIn = () => {
-        Animated.timing(
-            opacity,
-            {
-                toValue: 1,
-                duration: 300,
-                useNativeDriver: true,
-            }
-        ).start()
-
-        Animated.timing(
-            top,
-            {
-                toValue: 0,
-                duration: 800,
-                useNativeDriver: true,
-                easing: Easing.bounce,
-            }
-        ).start()
-    }
-
-    const fadeOut = () => {
-        Animated.timing(
-            opacity,
-            {
-                toValue: 0,
-                duration: 300,
-                useNativeDriver: true,
-            }
-        ).start()
-    }
+    const { opacity, position, fadeIn, fadeOut, startMovingPosition } = useAnimation()
 
     return (
         <View style={styles.container}>
@@ -45,13 +13,16 @@ export const Animation101Screen = () => {
                     ...styles.purpleBox, 
                     opacity, 
                     transform: [{
-                        translateY: top
+                        translateX: position
                     } ]
                 }} 
                 />
             <Button 
                 title="FadeIn"
-                onPress={ fadeIn }
+                onPress={ () => {
+                    fadeIn()
+                    startMovingPosition(100)
+                } }
             />
             <Button 
                 title="FadeOut"
