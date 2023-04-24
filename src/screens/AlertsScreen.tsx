b6687f9ff675c4f00c978e686f687b2ea2b29979
JorgeLpzGnz <geolg763@gmail.com>
2023-04-24 10:32:40 -0500

    205. Pompt ( Android && IOS )

--- src/screens/AlertsScreen.tsx
+++ src/screens/AlertsScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Alert, Button, View } from 'react-native'
+import prompt from 'react-native-prompt-android';
 import { HeaderTitle } from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
 
@@ -25,18 +26,34 @@ export const AlertsScreen = () => {
             });
     }
 
-    // Solo disponible en IOS
-
+    
     const showPrompt = () => {
 
-        Alert.prompt(
-            '¿Esta seguro?',
-            'Esta accion no se puede revertir',
-            ( value: string ) => console.log('info: ', value ),
-            'plain-text',
-            'hola mundo',
-            'email-address'
-        )
+        // Solo disponible en IOS
+        
+        // Alert.prompt(
+        //     '¿Esta seguro?',
+        //     'Esta accion no se puede revertir',
+        //     ( value: string ) => console.log('info: ', value ),
+        //     'plain-text',
+        //     'hola mundo',
+        //     'email-address'
+        // )
+
+        prompt(
+            'Enter password',
+            'Enter your password to claim your $1.5B in lottery winnings',
+            [
+             {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+             {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
+            ],
+            {
+                type: 'secure-text',
+                cancelable: false,
+                defaultValue: 'test',
+                placeholder: 'placeholder'
+            }
+        );
 
     }
 
@@ -47,9 +64,12 @@ export const AlertsScreen = () => {
                 title="Mostrar Alerta"
                 onPress={showAlert}
             />
+
+            <View style={{ height: 10 }}/>
+
             <Button
                 title="Mostrar Prompt"
-                onPress={showAlert}
+                onPress={showPrompt}
             />
         </View>
     )
