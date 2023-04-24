7be37a0ee851f6bc8226fc1a3133f5978e433ecc
JorgeLpzGnz <geolg763@gmail.com>
2023-04-24 16:18:20 -0500

    215. Modal ( Styles )

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -26,13 +26,26 @@ export const ModalScreen = () => {
                     // height: 100,
                     // width: 100,
                     backgroundColor: 'rgba(0,0,0,0.3)',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                 }}>
                     {/* Contenido del modal */}
                     <View style={{
                         backgroundColor: 'white',
+                        width: 200,
+                        height: 200,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        shadowOffset: {
+                            width: 0,
+                            height: 10,
+                        },
+                        shadowOpacity: 0.25,
+                        elevation: 10,
+                        borderRadius: 5,
                     }}>
-                        <HeaderTitle title="Modal"/>
-                        <Text>Cuerpo del modal</Text>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold',}}>Modal</Text>
+                        <Text style={{ fontSize: 16, fontWeight: '300', marginBottom: 20 }}>Cuerpo del modal</Text>
                         <Button
                             title="Cerrar"
                             onPress={ () => setIsVisible(false)}
