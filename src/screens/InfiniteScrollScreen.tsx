4e7245bbe2e0f9cf2810e330866200614a2f669a
JorgeLpzGnz <geolg763@gmail.com>
2023-04-25 09:51:39 -0500

    217. Animated Image

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native'
 import { HeaderTitle } from '../components/HeaderTitle'
+import { FadeInImage } from '../components/FadeInImage'
 
 export const InfiniteScrollScreen = () => {
 
@@ -12,25 +13,59 @@ export const InfiniteScrollScreen = () => {
 
         for (let i = 0; i < 5; i++) newArray[i] = numbers.length + i;
 
-        setNumbers([...numbers, ...newArray])
+        setTimeout(() => {
+            setNumbers([...numbers, ...newArray])
+        }, 1500);
+
 
     }
 
-    const renderItem = ( item: number ) => (
-        <Text style={ styles.textItem }>
-            { item }
-        </Text>
-    )
+    const renderItem = ( item: number ) => {
+
+        return (
+            <FadeInImage 
+                uri={`https://picsum.photos/id/${ item }/1024/1024`}
+                style={{
+                    width: '100%',
+                    height: 400,
+                }}
+            />
+        )
+
+        // return (
+        //     <Image
+        //         source={{ uri: `https://picsum.photos/id/${ item }/500/400`}}
+        //         style={{
+        //             width: '100%',
+        //             height: 400,
+        //         }}
+        //     />
+        // )
+    }
 
     return (
-        <View style={{ flex: 1, backgroundColor: 'red',}}>
+        <View style={{ flex: 1,}}>
             <FlatList
                 data={ numbers }
                 keyExtractor={ ( item ) => item.toString() }
                 renderItem={ ({ item }) => renderItem(item) }
-                ListHeaderComponent={<HeaderTitle title="Infinite Scroll" />}
+                ListHeaderComponent={() => (
+                    <View style={{ marginHorizontal: 20,}}>
+                        <HeaderTitle title="Infinite Scroll" />
+                    </View>
+                )}
                 onEndReached={ loadMore }
                 onEndReachedThreshold={0.5}
+                ListFooterComponent={ () => (
+                    <View style={{
+                        height: 150,
+                        width: '100%',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                        <ActivityIndicator size={ 25 } color="#5855d6"/>
+                    </View>
+                )}
             />
         </View>
     )
